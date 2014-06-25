6305763c740c4d2c6ba8c7b8ad2c9439d8374c2d
rfraposa <rich@hortonworks.com>
2014-06-24 21:28:24 -0600

    Added avro install to build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 . /root/dockerfiles/start_scripts/build.sh $@ # && (echo "Parent build.sh failed"; exit 1)
 
 #Add custom tasks for Pig/Hive course here...
-
+apt-get -y install python-setuptools
+easy_install avro
 
 echo -e "\n*** The lab environment has successfully been built for this classroom VM ***\n"
