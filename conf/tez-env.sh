59b7ddee7fae534e4ba8ff648f3fbe77aae9b150
rfraposa <rich@hortonworks.com>
2014-08-14 16:11:39 -0600

    Added necessary Tez config files

---
+++ conf/tez-env.sh
@@ -0,0 +1,8 @@
+# Tez specific configuration
+export TEZ_CONF_DIR=/etc/tez/conf
+
+# Set HADOOP_HOME to point to a specific hadoop install directory
+export HADOOP_HOME=${HADOOP_HOME:-/usr}
+
+# The java implementation to use.
+export JAVA_HOME=/usr/java/default
