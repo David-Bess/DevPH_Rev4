5d0faa6f77bae81ad4ecec1249223bb653db881f
Michael Bingham <michael.b.b.bingham@gmail.com>
2014-08-13 17:12:53 -0700

    Modified to use special hdp_hive_node to have Hive use MySQL

--- build.sh
+++ build.sh
@@ -2,8 +2,18 @@
 
 . /root/dockerfiles/start_scripts/build.sh $@ # && (echo "Parent build.sh failed"; exit 1)
 
+#Build hdp_hive_node
+echo -e "\n*** Building hwux/hdp_hive_node ***\n"
+cd /root/dockerfiles/hdp_hive_node
+docker build -t hwxu/hdp_hive_node .
+echo -e "\n*** Build of hwxu/hdp_hive_node complete! ***\n"
+
 #Add custom tasks for Pig/Hive course here...
 apt-get -y install python-setuptools
 easy_install avro
+
+apt-get update
+apt-get -y install mysql-client
+echo "alias mysql='mysql -h hiveserver'" >> /etc/profile
 
 echo -e "\n*** The lab environment has successfully been built for this classroom VM ***\n"
--- scripts/dev_cluster.sh
+++ scripts/dev_cluster.sh
@@ -23,7 +23,7 @@ echo "ResourceManager running on $IP_resourcemanager"
 
 #Start the Hive/Oozie Server
 echo "Starting a Hive/Oozie server..."
-CID_hive=$(docker run -d --privileged --link namenode:namenode -e namenode_ip=$IP_namenode -e NODE_TYPE=hiveserver --dns 8.8.8.8 -p 11000:11000 -p 2181 -p 50111:50111 -p 9083 -p 10000 -p 9999:9999 -p 9933:9933 -p 22  --name hiveserver -h hiveserver -i -t hwxu/hdp_node bash)
+CID_hive=$(docker run -d --privileged --link namenode:namenode -e namenode_ip=$IP_namenode -e NODE_TYPE=hive --dns 8.8.8.8 -p 11000:11000 -p 2181 -p 50111:50111 -p 9083 -p 3306 -p 10000 -p 9999:9999 -p 9933:9933 -p 22  --name hiveserver -h hiveserver -i -t hwxu/hdp_hive_node bash)
 IP_hive=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" hiveserver)
 echo "Hive/Oozie running on $IP_hive"
 
